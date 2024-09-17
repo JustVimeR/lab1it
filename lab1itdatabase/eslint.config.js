2012b38bcab2a05e7830a8c392d4b1215afe8192
Vlad <dog792804@gmail.com>
2024-09-17 18:01:00 +0300

    fix lint

--- lab1itdatabase/eslint.config.js
+++ lab1itdatabase/eslint.config.js
@@ -28,10 +28,18 @@ export default [
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+      "react/jsx-no-target-blank": "off",
+      "react-refresh/only-export-components": 0,
+      "react/prop-types": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "no-useless-catch": "off",
+      "no-unused-vars": "warn",
+      "vitest/expect-expect": "off",
+      "vitest/max-nested-describe": [
+        "error",
+        {
+          max: 0,
+        },
       ],
     },
   },
--- lab1itdatabase/src/components/MainTable.jsx
+++ lab1itdatabase/src/components/MainTable.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Table } from "@radix-ui/themes";
 import * as Dialog from '@radix-ui/react-dialog';
 import { saveToLocalStorage } from '../utils/localStorageUtils';
