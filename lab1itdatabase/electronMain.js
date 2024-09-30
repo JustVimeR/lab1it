586ab3328eeead242a4c212ac4407d7a214487d7
Vlad <dog792804@gmail.com>
2024-09-30 23:54:52 +0300

    make new dialog

--- lab1itdatabase/electronMain.js
+++ lab1itdatabase/electronMain.js
@@ -1,34 +1,66 @@
-import { app, BrowserWindow } from 'electron';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import { app, BrowserWindow, Menu, ipcMain } from "electron";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-function createWindow() {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
-            nodeIntegration: true,
-            contextIsolation: false,
-        },
-    });
+let win;
 
-    win.loadURL('http://localhost:5173');
+function createWindow() {
+	win = new BrowserWindow({
+		width: 800,
+		height: 600,
+		titleBarStyle: "hiddenInset",
+		webPreferences: {
+			preload: path.join(__dirname, "preload.js"),
+			nodeIntegration: false,
+			contextIsolation: true,
+		},
+	});
+
+	win.loadURL("http://localhost:5173");
+
+	win.webContents.openDevTools();
 }
 
-app.on('ready', createWindow);
+const createMenu = () => {
+	const menuTemplate = [
+		{
+			label: "Menu",
+			submenu: [
+				{
+					label: "Open Column Editor",
+					click: () => {
+						win.webContents.send("open-column-editor");
+					},
+				},
+				{ role: "quit" },
+			],
+		},
+	];
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+	const menu = Menu.buildFromTemplate(menuTemplate);
+	Menu.setApplicationMenu(menu);
+};
+
+app.on("ready", () => {
+	createWindow();
+	createMenu();
 });
 
-app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
+app.on("window-all-closed", () => {
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
+});
+
+app.on("activate", () => {
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createWindow();
+	}
+});
+
+ipcMain.on("open-column-editor", () => {
+	win.webContents.send("open-column-editor");
 });
